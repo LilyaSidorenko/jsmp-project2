f9f67c55ce16e5e47076b95623d00ca53d14777a
Liliia_Sydorenko <lilya.sidorenko1@gmail.com>
2017-04-20 18:01:02 +0300

    Add unit test for validation

--- tests/module.spec.js
+++ tests/module.spec.js
@@ -1,16 +1,56 @@
-import { sayHello } from '../src/js/module/module';
-import { sayHi } from '../src/js/module/module2';
-describe('module', function () {
-
-    const hello = sayHello('World');
-    const hi = sayHi();
-
-    it('should display proper hello', function () {
-        expect(hello).toBe('Hello World!');
+describe('validation form', function() {
+  describe('email', function() {
+        it('empty', function() {
+          expect(validateEmail('')).toBe(false);
+        });
+        it('not empty not valid', function() {
+          expect(validateEmail('liliia.com.ua')).toBe(false);
+        });
+        it('valid email', function() {
+          expect(validateEmail('liliia@com.ua')).toBe(true);
+        });
     });
-
-    it('should display proper hi', function () {
-        expect(hi).toBe('hi');
+  describe('fullName', function() {
+    it('empty', function() {
+      expect(validateName('')).toBe(false);
     });
-
+    it('not empty not letters', function() {
+      expect(validateName('12345')).toBe(false);
+    });
+    it('not empty not only letters', function() {
+      expect(validateName('liliia12345')).toBe(false);
+    });
+    it('not empty not full', function() {
+      expect(validateName('liliia')).toBe(false);
+    });
+    it('valid name', function() {
+      expect(validateName('liliia sydorenko')).toBe(true);
+    });
+  });
+  describe('phone number', function() {
+    it('empty', function() {
+      expect(validatePhone('')).toBe(false);
+    });
+    it('not empty not number', function() {
+      expect(validatePhone('my_number')).toBe(false);
+    });
+    it('not empty not only numbers', function() {
+      expect(validatePhone('my_number1234')).toBe(false);
+    });
+    it('not empty not valid', function() {
+      expect(validatePhone('9058473847')).toBe(false);
+    });
+    it('valid phone number', function() {
+      expect(validatePhone('0935647477')).toBe(true);
+    });
+  });
+  describe('massage', function() {
+    it('empty', function() {
+      expect(validateMessage('')).toBe(false);
+    });
+    it('valid message', function() {
+      expect(validateMessage('Some message for you')).toBe(true);
+    });
+  });
 });
+
